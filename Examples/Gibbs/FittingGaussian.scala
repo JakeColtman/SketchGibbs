0dc6667d52b2e45eb7159621321b303167dd70b1
Jake Coltman <jakecoltman@sky.com>
2017-09-26 12:51:44 +0100

    Add easier API for connecting a node to many nodes

--- Examples/Gibbs/FittingGaussian.scala
+++ Examples/Gibbs/FittingGaussian.scala
@@ -1,14 +1,10 @@
 package Examples.Gibbs
 import SumProduct._
-import breeze.stats.distributions.Gaussian
-/**
-  * Created by jacoltman on 24/09/2017.
-  */
+
 object FittingGaussian extends App {
 
   val sigma = VariableFactory("sigma")
   val theta = VariableFactory("theta")
-
 
   val observations = List(10.0, 9.0, 9.8)
   val variables = List(VariableFactory("x_1"), VariableFactory("x_2"), VariableFactory("x_3"))
@@ -20,13 +16,11 @@ object FittingGaussian extends App {
   val theta_distribution = DistributionFactory.gaussian(theta, 0.0, 10.0)
   val theta_node = NodeFactory(theta_distribution, 0.0)
 
-  val graph = GraphFactory.gibbs(List(theta_node, sigma_node) ++ observations_nodes)
-  graph.add_edges(theta_node->observations_nodes.head)
-  graph.add_edges(theta_node->observations_nodes(1))
-  graph.add_edges(theta_node->observations_nodes(2))
-  graph.add_edges(sigma_node->observations_nodes.head)
-  graph.add_edges(sigma_node->observations_nodes(1))
-  graph.add_edges(sigma_node->observations_nodes(2))
+  val graph = {
+    GraphFactory.gibbs(List(theta_node, sigma_node) ++ observations_nodes)
+  }
+  graph.add_edges(theta_node->observations_nodes)
+  graph.add_edges(sigma_node->observations_nodes)
   for(x <-1 to 100) {
     graph.run_iteration()
     println(theta_node.current_value)
--- SumProduct/Vertex.scala
+++ SumProduct/Vertex.scala
@@ -11,6 +11,10 @@ trait Vertex[NodeType] {
     EdgeFactory(Directed, this.content, vertex.content)
   }
 
+  def ->(vertices: List[Vertex[NodeType]]): List[Edge[NodeType]] = {
+    vertices.flatMap(v => EdgeFactory(Directed, this.content, v.content))
+  }
+
   def <->(vertex: Vertex[NodeType]): List[Edge[NodeType]] = {
     EdgeFactory(Undirected, this.content, vertex.content)
   }
@@ -19,14 +23,20 @@ trait Vertex[NodeType] {
 
 
 class VertexSpec extends FlatSpec with Matchers {
-  "Vertices " should " be able to created directed edges between themselves" in {
+  "Vertices " should " be able to create directed edges between themselves" in {
     val a = NodeFactory("a")
     val b = NodeFactory("b")
     a->b should be (EdgeFactory(Directed, a, b))
   }
-  "Vertices " should " be able to created undirected edges between themselves" in {
+  "Vertices " should " be able to create undirected edges between themselves" in {
     val a = NodeFactory("a")
     val b = NodeFactory("b")
     a <-> b should contain theSameElementsAs EdgeFactory(Undirected, a, b)
+  }
+  "Vertices " should " be able to generate to many other vertices quickly " in {
+    val a = NodeFactory("a")
+    val b = NodeFactory("b")
+    val c = NodeFactory("c")
+    a->List(b,c) should contain theSameElementsAs EdgeFactory(Directed, a, b) ++ EdgeFactory(Directed, a, c)
   }
 }
